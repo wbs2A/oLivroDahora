4fa554062afd9dc89a8393d86525fd8d66e6e51d
Wesley Barbosa <34480299+wbs2a@users.noreply.github.com>
2019-04-30 17:48:16 -0400

    Remoção firebase route

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,3 @@ Route::get('/', function () {
 
 Route::match(['get', 'post'], '/botman', 'BotManController@handle');
 Route::get('/botman/tinker', 'BotManController@tinker');
-Route::get('/test','FirebaseController@index');
